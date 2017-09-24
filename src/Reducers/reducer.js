f4553d438756de797faec9dd9df5281e0a4191a2
Daniel Stahl <danstahl1138@gmail.com>
2017-09-24 12:09:33 -0500

    cleanup

--- src/Reducers/reducer.js
+++ src/Reducers/reducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux'
-//import firebaseReducer from './firebaseReducer'
 import postReducer from './postReducer'
 import userReducer from './userReducer'
 
 export default combineReducers({
-    //firebaseReducer,
     posts:postReducer,
     user:userReducer
 })
